ec354894d993ddd8677b41add589523ee901c95e
PARAS <128474453+paras-0007@users.noreply.github.com>
2025-06-27 16:28:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -403,7 +403,7 @@ def run_app():
                 select_all_value = st.session_state.get('select_all_checkbox', False)
                 for _, row in df.iterrows(): st.session_state[f"select_{row['Id']}"] = select_all_value
             st.checkbox("Select/Deselect All", key="select_all_checkbox", on_change=toggle_all, args=(df_filtered,))
-            header_cols = st.columns([0.5, 3, 2, 1.5, 2, 1.5, 2])
+            header_cols = st.columns([0.5, 2.5, 2, 1.5, 2, 1.5, 2])
             header_cols[0].markdown("")
             header_cols[1].markdown("**Name**")
             header_cols[2].markdown("**Role**")
